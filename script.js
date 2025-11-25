664d9d156f5a80471d68a1928b6916c0653c5f1c
anideva <anidevarchowdhury@gmial.com>
2025-11-25 14:46:32 +0530

    latest changes in the js file

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const dropdown = document.getElementById("colorSelect");
+// get the button
+const button= document.getElementById("removeBtn");
+
+//function to remove the color
+function removeColor(){
+const index= dropdown.selectedIndex;
+if(index !== -1) {
+dropdown.remove(index);
+}
+}
+
+//add event listener
+button.addEventListener("click", removeColor);
